39b0426aaf23e18d912239cd00c0d83c23e51f38
CristiChirita <CristiChirita@github.com>
2018-02-06 11:03:46 +0000

    must add file io

--- CW2/SimpleRobotControl.py
+++ CW2/SimpleRobotControl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import sys
+import getopt
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from math import pow,atan2,sqrt,pi
@@ -9,24 +10,28 @@ from PyKDL import Rotation
 class stdr_controller():
 	def __init__(self, argv):
 		rospy.init_node('stdr_controller', anonymous=True)
-		velocity_publisher = rospy.Publisher('/robot0/cmd_vel', Twist, queue_size=10)
+		self.velocity_publisher = rospy.Publisher('/robot0/cmd_vel', Twist, queue_size=10)
 		current_pose_subscriber = rospy.Subscriber('/robot0/odom', Odometry, self.current_callback)
 		current_pose = Odometry()
+		inputfile = ""
+		
 		try:
-			args = getopt.getopt(argv)
+			args = getopt.getopt(argv, '')
+			for arg in args:
+				inputfile = arg
+				break
+			# fp = open(inputfile)
 		except getopt.GetoptError:
 			#sys.exit(2)
 			pass
-		for arg in args:
-			inputfile = arg
-		fp = open(inputfile)
+
 		distance_tolerance = 0.01
 		angular_tolerance = 5
 
 	def current_callback(self, data):
 		self.current_pose = data
 	
-	def run():
+	def run(self):
 		rospy.sleep(1.0)
 		
 		while not rospy.is_shutdown():
@@ -37,7 +42,7 @@ class stdr_controller():
 			orientation = pose.orientation
 			theta = 2 * atan2(orientation.z, orientation.w) * 180 / pi
 			
-			rospy.loginfo('Current position, x: {}, y: {}, theta: {}'.format(position.x, position.y, theta)
+			rospy.loginfo('Current position, x: {}, y: {}, theta: {}'.format(position.x, position.y, theta))
 			
 			try:
 				targetX = float(raw_input('Enter desired X coordinate: '))
@@ -48,17 +53,17 @@ class stdr_controller():
 				if targetX - position.x > 0: #target to the right
 					if theta != 0: 
 						if theta >= 180:
-							setAngle(5, theta, True)
+							self.setAngle(5, theta, True)
 						else:
-							setAngle(5, theta - 180, False)
-					move(1, targetX -  position.x)
+							self.setAngle(5, theta - 180, False)
+					self.move(1, targetX -  position.x)
 				else: #target to the left
 					if theta != 180:
 						if theta >= 180:
-							setAngle(5, 180 - theta, False)
+							self.setAngle(5, 180 - theta, False)
 						else:
-							setAngle(5, theta - 180, True)
-					move(1, position.x - targetX)
+							self.setAngle(5, theta - 180, True)
+					self.move(1, position.x - targetX)
 				
 				#Get new angle value
 				theta = self.getTheta()
@@ -67,28 +72,28 @@ class stdr_controller():
 				if targetY - position.y > 0: #target higher
 					if theta != 90:
 						if (theta < 90):
-							setAngle(5, 90 - theta, False)
+							self.setAngle(5, 90 - theta, False)
 						elif theta > 270:
-							setAngle(5, 360 - theta + 90, False)
+							self.setAngle(5, 360 - theta + 90, False)
 						else:
-							setAngle(5, theta - 90, True)
-					move(1, targetY - position.y)
+							self.setAngle(5, theta - 90, True)
+					self.move(1, targetY - position.y)
 				else: #target lower
 					if theta != 270:
 						if theta < 90:
-							setAngle(5, theta + 90, True)
+							self.setAngle(5, theta + 90, True)
 						elif theta > 270:
-							setAngle(5, theta - 270, True)
+							self.setAngle(5, theta - 270, True)
 						else:
-							setAngle(5, 270 - theta, False)
-					move(1, position.y - targetY)
+							self.setAngle(5, 270 - theta, False)
+					self.move(1, position.y - targetY)
 				
 				#Adjust to specified angle
 				theta = self.getTheta()
 				if targetAngle > theta:
-					setAngle(5, targetAngle - theta, False)
+					self.setAngle(5, targetAngle - theta, False)
 				else:
-					setAngle(5, theta - targetAngle, True)
+					self.setAngle(5, theta - targetAngle, True)
 						
 			except ValueError:
 				rospy.loginfo('Illegal value entered')
@@ -97,13 +102,14 @@ class stdr_controller():
 			vel_msg.linear.z = 0.0
 			self.velocity_publisher.publish(vel_msg)
 	
-	def getTheta(self)
+	def getTheta(self):
 		pose = self.current_pose.pose.pose
 		orientation = pose.orientation
 		theta = 2 * atan2(orientation.z, orientation.w) * 180 / pi
 		return theta
 	
-	def setAngle(speed, angle, clockwise):
+	def setAngle(self, speed, angle, clockwise):
+		vel_msg = Twist()
 		angular_speed = speed*2*pi/360
 		relative_angle = angle*2*pi/360
 		
@@ -121,15 +127,16 @@ class stdr_controller():
 		current_angle = 0
 	
 		while(current_angle < relative_angle):
-			velocity_publisher.publish(vel_msg)
+			self.velocity_publisher.publish(vel_msg)
 			t1 = rospy.Time.now().to_sec()
 			current_angle = angular_speed*(t1 - t0)
 	
 		vel_msg.angular.z = 0
-		velocity_publisher.publish(vel_msg)
-		rospy.spin()
+		self.velocity_publisher.publish(vel_msg)
 	
-	def move(speed, distance):
+	def move(self, speed, distance):
+		vel_msg = Twist()
+		vel_msg.linear.x = speed
 		vel_msg.linear.y = 0
 		vel_msg.linear.z = 0
 		vel_msg.angular.x = 0
@@ -140,15 +147,15 @@ class stdr_controller():
 		current_distance = 0
 	
 		while (current_distance < distance):
-			velocity_publisher.publish(vel_msg)
+			self.velocity_publisher.publish(vel_msg)
 			t1=rospy.Time.now().to_sec()
 			current_distance = speed * (t1-t0)
 	
 		vel_msg.linear.x = 0
-		velocity_publisher.publish(vel_msg)
+		self.velocity_publisher.publish(vel_msg)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 	try:
 		x = stdr_controller(sys.argv[1:])
 		x.run()
-	except: rospy.ROSInterruptException: pass
+	except rospy.ROSInterruptException: pass
